395d55581fb9dea1cf28bb8933b8da2bbc10b120
Alex Siegberg and Tae Lee <alex@>
2019-06-03 15:44:33 -0700

    practice for independent function

--- Whiskey/js/scripts.js
+++ Whiskey/js/scripts.js
@@ -1,46 +1,37 @@
-// $(function() {
-//   var userFavWhiskey = prompt("What is your favorite whiskey?");
-//   var whiskeyPrice = parseFloat(prompt("How much does it cost?"));
-// })
-
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
 
-  $("h2").click(function() {
-    alert("This is a header2.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-
-var sentence = prompt("What is your favorite whiskey?");
+  var sentence = prompt("What is your favorite whiskey?");
 
   var userFavWhiskey = function(sentence) {
     console.log(sentence);
-    letterCase(sentence);
-};
+    alert(letterCase(sentence));
+    alert(reverseLetter(sentence));
+    alert(addLetter(sentence));
+    alert(numLetter(sentence));
+  };
 
   var letterCase = function(sentence) {
     var firstLetter = sentence.charAt(0).toUpperCase();
     var lastLetter = sentence.charAt(sentence.length - 1).toUpperCase();
     var newSentence = firstLetter + lastLetter;
-    alert("new letters are " + newSentence);
-    reverseLetter(newSentence);
-  }
+    return newSentence;
+  };
 
-  var reverseLetter = function(letterCase) {
-    var tempLetter = letterCase.charAt(1) + letterCase.charAt(0);
-    alert("reversed letters are " + tempLetter)
-    addLetter(tempLetter);
-  }
+  var reverseLetter = function(sentence) {
+    var reverse = sentence.charAt(sentence.length-1).toUpperCase() + sentence.charAt(0).toUpperCase();
+    return reverse;
+  };
 
-  var addLetter = function(reverseLetter) {
-    alert("added letters are " + sentence + reverseLetter)
-  }
+  var addLetter = function(sentence) {
+    var addLetterSentence = sentence + reverseLetter(sentence);
+    return addLetterSentence;
+  };
 
-userFavWhiskey(sentence);
+  var numLetter = function(sentence) {
+    var index = sentence.charAt(sentence.length/2);
+    return index + sentence + reverseLetter(sentence);
+  };
+
+  userFavWhiskey(sentence);
 
 });
