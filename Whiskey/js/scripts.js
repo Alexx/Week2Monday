c4347aec1cc0a8c7ce104055178a7c47b9cf5ac7
Alex Siegberg and Tae Lee <tae@>
2019-06-03 15:54:21 -0700

    practice for global constant independent function

--- Whiskey/js/scripts.js
+++ Whiskey/js/scripts.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
 
-  var sentence = prompt("What is your favorite whiskey?");
+  const sentence = prompt("What is your favorite whiskey?");
 
-  var userFavWhiskey = function(sentence) {
+  var userFavWhiskey = function() {
     console.log(sentence);
   };
 
-  var letterCase = function(sentence) {
+  var letterCase = function() {
     var firstLetter = sentence.charAt(0).toUpperCase();
     var lastLetter = sentence.charAt(sentence.length - 1).toUpperCase();
     var newSentence = firstLetter + lastLetter;
     return newSentence;
   };
 
-  var reverseLetter = function(sentence) {
+  var reverseLetter = function() {
     var reverse = sentence.charAt(sentence.length-1).toUpperCase() + sentence.charAt(0).toUpperCase();
     return reverse;
   };
 
-  var addLetter = function(sentence) {
-    var addLetterSentence = sentence + reverseLetter(sentence);
+  var addLetter = function() {
+    var addLetterSentence = sentence + reverseLetter();
     return addLetterSentence;
   };
 
-  var numLetter = function(sentence) {
+  var numLetter = function() {
     var index = sentence.charAt(sentence.length/2);
-    return index + sentence + reverseLetter(sentence);
+    return index + sentence + reverseLetter();
   };
 
-  userFavWhiskey(sentence);
-  alert(letterCase(sentence));
-  alert(reverseLetter(sentence));
-  alert(addLetter(sentence));
-  alert(numLetter(sentence));
+  userFavWhiskey();
+  alert(letterCase());
+  alert(reverseLetter());
+  alert(addLetter());
+  alert(numLetter());
 
 });
